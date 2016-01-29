bd191e01e6fbf40e71b6b757c6754bf0ccaee51e
zoonie <zooonie@outlook.com>
2016-01-29 01:06:24 +0000

    Only stop and remove sound if it's playing

--- java/com/dynious/soundscool/client/audio/SoundPlayer.java
+++ java/com/dynious/soundscool/client/audio/SoundPlayer.java
@@ -46,8 +46,11 @@ public class SoundPlayer
 
     public void stopSound(String identifier)
     {
-    	soundSystem.stop(identifier);
-    	playing.remove(identifier);
+    	if(soundSystem.playing(identifier))
+    	{
+    		soundSystem.stop(identifier);
+    		playing.remove(identifier);
+    	}
     }
     
     public void stopSounds()
